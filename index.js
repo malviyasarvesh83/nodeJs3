1840455ab7c4f8cab8e6fda91c3a51a35a253825
malviyasarvesh83 <malviyasarvesh83@gmail.com>
2023-02-23 18:24:57 +0530

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const http = require("http");
-const routes = require("./routes");
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(
+      '<form action="/product" method="POST"><div style="margin-top: 10px;"><div style="margin-top: 10px;"><input type="text" name="name"></div><div style="margin-top: 10px;"><input type="number" name="size"></div><div style="margin-top: 10px;"><button type="submit">Add Product</button></div></div></form>'
+    );
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
-app.use((req, res, next) => {
-    console.log('In the another middleware!');
-    res.send('<h1>Hello from Express</h1>');
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome to Express!!</h1>');
 })
-
-const server = http.createServer(app);
 
 const PORT = 8000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server is successfully running on PORT ${PORT}`);
 });
